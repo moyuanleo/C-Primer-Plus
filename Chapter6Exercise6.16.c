c7ffdb5232ac73a5b7413ef61c466e810867b236
Xuqiming <1357474476@qq.com>
2021-02-13 17:00:11 +0800

    Add files via upload

---
+++ Chapter6Exercise6.16.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main(void) {
+	float daphne, deirdre;
+	daphne = deirdre = 100.0;
+	int year = 0;
+	do {
+		daphne = daphne + 100 * 0.1;
+		deirdre = deirdre + deirdre * 0.05;
+		year++;
+	} while ((deirdre - daphne) < 0);
+	printf("%d years later.\nDaphne = %f.\nDeirdre = %f.\n",
+		year, daphne, deirdre);
+	printf("Done!\n");
+	return 0;
+}
