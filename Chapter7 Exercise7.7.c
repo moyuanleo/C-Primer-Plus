286803166798a2343c3a35e0ff627948bcd83de2
Xuqiming <1357474476@qq.com>
2021-02-15 13:40:17 +0800

    Add files via upload

---
+++ Chapter7 Exercise7.7.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#define BASE_SALARY 10
+#define EXRA_HOUR 1.5
+#define BASE_TAX 0.15
+#define EXTRA_TAX 0.2
+#define EXCEED_TAX 0.25
+
+int main(void)
+{
+	float hours = 0;
+	float salary, tax, taxed_salary;
+	printf("Enter the working hours a week:\n");
+	scanf_s("%f", &hours);
+	if (hours <=0)
+		return 0;
+	if (hours <= 30) {
+		salary = hours * BASE_SALARY;
+		tax = salary * BASE_TAX;
+		taxed_salary = salary - tax;
+	}
+	else if (hours <= 40) {
+		salary = hours * BASE_SALARY;
+		tax = 300 * BASE_TAX + (salary - 300) * EXTRA_TAX;
+		taxed_salary = salary - tax;
+	}
+	else {
+		salary = (40 + (hours - 40) * EXTRA_TAX) * BASE_SALARY;
+		if (salary <= 450) tax = 300 * BASE_TAX + (salary - 300) * EXTRA_TAX;
+		else tax = 300 * BASE_TAX + (salary - 300) * EXTRA_TAX + (salary - 450) * EXCEED_TAX;
+		taxed_salary = salary - tax;
+	}
+	printf("You salary before tax is %.2f,tax is %.2f,salary after tax is %.2f\n",
+		salary, tax, taxed_salary);
+	printf("Done!\n");
+	return 0;
+}
